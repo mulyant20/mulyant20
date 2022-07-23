b943a97dcd9b7ac6ec02eaf0060f2e370e77a684
mulyant20 <96921234+mulyant20@users.noreply.github.com>
2022-07-23 20:32:42 +0000

    Deploying to gh-pages from @ mulyant20/mulyant20.github.io@b7006e91b06d80480da141ca38d9d42a7866e7da 🚀

--- _next/static/l7MOuUptFy_pBRetGP0pP/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-afb0ee61ebb67e6c.js"],"/_error":["static/chunks/pages/_error-7397496ca01950b1.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB()
